720e94ef4e7c4b42435e190a07d71aba532f97fd
Youssef Ashraf <youseefashraf54@gmail.com>
2025-12-10 01:57:22 +0200

    Updated service

--- Milo-Backend/src/main/java/com/app/milobackend/dtos/MailDTO.java
+++ Milo-Backend/src/main/java/com/app/milobackend/dtos/MailDTO.java
@@ -5,17 +5,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MailDTO {
+    private Long id;
+    private String folder;
     private String sender;
-    private String receiver;
+    private String senderEmail;
+    private List<String> receiverEmail;
+    private String time;
     private String subject;
     private String body;
-    private int priority;
     private List<MultipartFile> attachments;
+    private boolean read;
+    private boolean active;
+    private boolean starred;
+    private boolean hasAttachment;
+    private int priority;
 
 }
---
+++ Milo-Backend/src/main/java/com/app/milobackend/repositories/FolderRepo.java
@@ -0,0 +1,7 @@
+package com.app.milobackend.repositories;
+
+import com.app.milobackend.models.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepo extends JpaRepository<Folder,Long> {
+}
