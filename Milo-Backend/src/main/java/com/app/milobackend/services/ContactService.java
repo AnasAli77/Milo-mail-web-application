27dff3815be5a5283726d1dc348c749ec01caed0
Mostafa Fathy Ali Amer <96601097+tofyfathy12@users.noreply.github.com>
2025-12-13 02:12:57 +0200

    wla 7aga

--- Milo-Backend/src/main/java/com/app/milobackend/services/ContactService.java
+++ Milo-Backend/src/main/java/com/app/milobackend/services/ContactService.java
@@ -2,6 +2,8 @@ package com.app.milobackend.services;
 
 import com.app.milobackend.models.Contact;
 import com.app.milobackend.repositories.ContactRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,10 +11,15 @@ import java.util.List;
 @Service
 public class ContactService {
 
+    @Autowired
     private ContactRepo contactRepo;
 
-    public ContactService (ContactRepo contactRepo) {
-        this.contactRepo = contactRepo;
+    public String getCurrentUserEmail() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            return authentication.getName();
+        }
+        return null; // Or throw an exception
     }
 
     public Contact storeContact(Contact contact) {
